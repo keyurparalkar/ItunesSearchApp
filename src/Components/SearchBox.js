07525c4d1325379c40c85a9553f8d7883eceba03
keyurparalkar <keur.plkar@gmail.com>
2020-10-11 08:19:20 +0530

    Code refactoring & basic layout added

---
+++ src/Components/SearchBox.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default class SearchBox extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+            <div>
+                <input type="text" />
+            </div>
+        );
+    }
+}
---
+++ src/Components/TileContainer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const TileContainer = ()=> (
+    <div className="title-container">
+          <ul>
+            <li key="1">Song 1</li>
+            <li key="2">Song 2</li>
+            <li key="3">Song 3</li>
+            <li key="4">Song 4</li>
+            <li key="5">Song 5</li>
+            <li key="6">Song 6</li>
+          </ul>
+      </div>
+);
+
+export default TileContainer;
